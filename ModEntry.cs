cc1e96664736ae36b76e7014b1b9aac064764033
justastrangerlica <justastrangerlica@gmail.com>
2025-02-13 09:31:34 -0600

    Implement translation support for config screen

--- ModEntry.cs
+++ ModEntry.cs
@@ -37,7 +37,7 @@ namespace BetterBombs
                 prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.onExplosion_Prefix))
             );
 
-            helper.Events.GameLoop.GameLaunched += (sender, args) => { 
+            helper.Events.GameLoop.GameLaunched += (sender, args) => {
 
                 if (helper.ModRegistry.IsLoaded("mistyspring.ItemExtensions"))
                 {
@@ -48,20 +48,20 @@ namespace BetterBombs
                 {
                     var gmcmApi = helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
 
-                    gmcmApi?.Register(ModManifest, () => config = new ModConfig(), () => helper.WriteConfig(config));
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.DamageFarmers, (damageFarmers) => config.DamageFarmers = damageFarmers, () => "Damage Farmers", () => "Toggles whether farmers are damaged by explosions. Covers *all* explosions.");
-                    gmcmApi?.AddNumberOption(ModManifest, () => config.Damage, (damage) => config.Damage = damage, () => "Damage Multiplier", () => "Scales the damage inflicted by explosions. Affects damage to monsters and farmers if Damage Farmers is enabled. 2x damage is enough for mega bombs to kill almost anything.", 0.1f, 10f, 0.1f);
-                    gmcmApi?.AddNumberOption(ModManifest, () => config.Radius, (radius) => config.Radius = radius, () => "Explosion Radius Multiplier", () => "Scales the size of explosions. Overly large values may cause visual glitches.", 0.1f, 5f, 0.1f);
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.BreakClumps, (breakClumps) => config.BreakClumps = breakClumps, () => "Break Clumps", () => "Toggles whether explosions are able to break Resource Clumps. Currently, clumps are simply removed and resources spawned in their place instead of playing a breaking animation. Acts as a master switch for the granular clump-breaking settings, if this is disabled they're all disabled.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.BreakStoneClumps, (breakStoneClumps) => config.BreakStoneClumps = breakStoneClumps, () => "Break Stone Clumps", () => "Toggles whether explosions are able to break stone Resource Clumps. This includes all boulders.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.BreakWoodClumps, (breakWoodClumps) => config.BreakWoodClumps = breakWoodClumps, () => "Break Wood Clumps", () => "Toggles whether explosions are able to break wooden Resource Clumps. This includes large stumps and hollow logs.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.BreakWeedsClumps, (breakWeedsClumps) => config.BreakWeedsClumps = breakWeedsClumps, () => "Break Weeds Clumps", () => "Toggles whether explosions are able to break weeds Resource Clumps. This includes the two large green rain grasses.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.BreakOtherClumps, (breakOtherClumps) => config.BreakOtherClumps = breakOtherClumps, () => "Break Other Clumps", () => "Toggles whether explosions are able to break miscellaneous Resource Clumps. There are none in vanilla Stardew Valley, only those added by mods.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.CollectMinerals, (collectMinerals) => config.CollectMinerals = collectMinerals, () => "Collect Minerals", () => "Toggles whether grabbable minerals in the explosion radius are dropped instead of being destroyed.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.CollectForage, (collectForage) => config.CollectForage = collectForage, () => "Collect Forage", () => "Toggles whether grabbable forage in the explosion radius is dropped instead of being destroyed. Respects foraging level and professions.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.CollectFish, (collectFish) => config.CollectFish = collectFish, () => "Collect Fish Forage", () => "Toggles whether grabbable fish and shells in the explosion radius are dropped instead of being destroyed. Respects foraging level and professions.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.CollectedQuality, (collectedQuality) => config.CollectedQuality = collectedQuality, () => "Collected Items Have Quality", () => "Toggles whether collecting forage or fish with bombs will give it a quality level. Enabled by default.");
-                    gmcmApi?.AddBoolOption(ModManifest, () => config.CollectedDegrade, (collectedDegrade) => config.CollectedDegrade = collectedDegrade, () => "Degrade Collected Items", () => "Toggles whether quality levels are reduced by one when collecting items via explosion. Disabled by default.");
+                    gmcmApi?.Register(ModManifest, () => Config = new ModConfig(), () => helper.WriteConfig(Config));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.DamageFarmers, (damageFarmers) => Config.DamageFarmers = damageFarmers, () => i18n.Get("DamageFarmers.name"), () => i18n.Get("DamageFarmers.description"));
+                    gmcmApi?.AddNumberOption(ModManifest, () => Config.Damage, (damage) => Config.Damage = damage, () => i18n.Get("Damage.name"), () => i18n.Get("Damage.description"), 0.1f, 10f, 0.1f);
+                    gmcmApi?.AddNumberOption(ModManifest, () => Config.Radius, (radius) => Config.Radius = radius, () => i18n.Get("Radius.name"), () => i18n.Get("Radius.description"), 0.1f, 5f, 0.1f);
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.BreakClumps, (breakClumps) => Config.BreakClumps = breakClumps, () => i18n.Get("BreakClumps.name"), () => i18n.Get("BreakClumps.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.BreakStoneClumps, (breakStoneClumps) => Config.BreakStoneClumps = breakStoneClumps, () => i18n.Get("BreakStoneClumps.name"), () => i18n.Get("BreakStoneClumps.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.BreakWoodClumps, (breakWoodClumps) => Config.BreakWoodClumps = breakWoodClumps, () => i18n.Get("BreakWoodClumps.name"), () => i18n.Get("BreakWoodClumps.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.BreakWeedsClumps, (breakWeedsClumps) => Config.BreakWeedsClumps = breakWeedsClumps, () => i18n.Get("BreakWeedsClumps.name"), () => i18n.Get("BreakWeedsClumps.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.BreakOtherClumps, (breakOtherClumps) => Config.BreakOtherClumps = breakOtherClumps, () => i18n.Get("BreakOtherClumps.name"), () => i18n.Get("BreakOtherClumps.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.CollectMinerals, (collectMinerals) => Config.CollectMinerals = collectMinerals, () => i18n.Get("CollectMinerals.name"), () => i18n.Get("CollectMinerals.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.CollectForage, (collectForage) => Config.CollectForage = collectForage, () => i18n.Get("CollectForage.name"), () => i18n.Get("CollectForage.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.CollectFish, (collectFish) => Config.CollectFish = collectFish, () => i18n.Get("CollectFish.name"), () => i18n.Get("CollectFish.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.CollectedQuality, (collectedQuality) => Config.CollectedQuality = collectedQuality, () => i18n.Get("CollectedQuality.name"), () => i18n.Get("CollectedQuality.description"));
+                    gmcmApi?.AddBoolOption(ModManifest, () => Config.CollectedDegrade, (collectedDegrade) => Config.CollectedDegrade = collectedDegrade, () => i18n.Get("CollectedDegrade.name"), () => i18n.Get("CollectedDegrade.description"));
                 }
             };
         }
